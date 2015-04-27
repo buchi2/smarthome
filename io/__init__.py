8030aeebc49266cbeb4ea9da9c98866d7f00fe9c
aschwith <alex.schwithal@gmx.de>
2015-04-27 19:50:22 +0200

    fixed wrong variable names

--- io/__init__.py
+++ io/__init__.py
@@ -31,7 +31,7 @@ items.conf
 [test]
 [[io]]
 type = bool
-io_pin = 24
+input_pin = 24
 io_edge = rising
 
 
@@ -55,7 +55,7 @@ class IO():
         self.pin = pin
         self.edge = int(edge)
         self._ports = dict()
-        logger.info('IO: Configured for port {0} and edge{1}'.format(self.pin, self.edge))
+        logger.info('IO: Configured for port {0} and edge {1}'.format(self.pin, self.edge))
         self.run
         self._sh = smarthome
 
@@ -96,8 +96,8 @@ class IO():
         logger.debug('IO: Module stopped')
 
     def parse_item(self, item):
-        if 'io_pin' in item.conf:
-            self._ports[item.conf['io_pin']] = item
+        if 'input_pin' in item.conf:
+            self._ports[item.conf['input_pin']] = item
             logger.debug("IO: Parse item: {0}".format(item))
             return self.update_item
         else:
@@ -110,7 +110,7 @@ class IO():
     def update_item(self, item, caller=None, source=None, dest=None):
         logger.debug("IO: update function started")
         if caller != 'io_detect':
-            if 'io_pin' in item.conf:
+            if 'input_pin' in item.conf:
                 self.set_port(item.conf['netio_port'], item())
                 logger.debug('IO: Port updated externally')
 
